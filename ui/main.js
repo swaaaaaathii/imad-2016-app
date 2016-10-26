7a0a9cd9c6308cc9c7bad641a699e266f1bf2a3d
swaaaaaathii <swathi.n201097@gmail.com>
2016-10-26 18:49:32 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -3,7 +3,7 @@ console.log('Loaded!');
 var element=document.getElementById('main-text');
 element.innerHTML = 'new text';
 
-var image=document.getElementByID('img');
+var image=document.getElementById('img');
 image.onclick = function () { 
     image.style.marginLeft = '100px';
 };
