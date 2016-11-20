d45cfb73e11d5c1a2bb3a3f432deae8e98d6da9f
swaaaaaathii <swathi.n201097@gmail.com>
2016-11-20 17:59:43 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -107,6 +107,18 @@ app.post('/create-user',function(req,res){
     });
 });
 
+app.post('/login',function(req,res){
+    var username = req.body.username;
+    var password = req.body.password;
+    pool.query('INSERT into "user" (username,password) values ($1,$2)',[username,dbstring],function(err,result){
+        if(err){
+            res.status(500).send(err.toString());
+        }else{
+            res.send('User successfully created : ' + username);
+        }
+    });
+});
+
 /*app.get('/:articleName',function (req,res){
    var articleName = req.params.articleName;
    res.send(createTemplate(articles[articleName])); 
