eef000b9f21d54c66b216b0e87d252ba2fc6151b
swaaaaaathii <swathi.n201097@gmail.com>
2016-11-20 18:07:51 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -114,6 +114,9 @@ app.post('/login',function(req,res){
         if(err){
             res.status(500).send(err.toString());
         }else{
+            if(result.rows.length===0){
+                res.status(400).send('Invalid username/password');
+            }
             res.send('User successfully created : ' + username);
         }
     });
