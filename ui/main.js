1e266d30d76985e021d46e406039b099797b4e6d
swaaaaaathii <swathi.n201097@gmail.com>
2016-11-21 09:14:10 +0530

    Update main.js

--- ui/main.js
+++ ui/main.js
@@ -1,5 +1,30 @@
 console.log('Loaded!');
 
+var login=document.getElementById('login_button');
+
+login.onclick = function(){
+    var request = new XMLHttpRequest();
+    request.onreadystatechange = function(){
+        if(request.readyState === XMLHttpRequest.DONE){
+            if(request.status===200){
+                alert('Logged in successfully');
+            }else if(request.status===403){
+                alert('Username/password incorrect');
+            }else if(request.status===500){
+                alert('Something wrong with the server');
+            }
+        }
+    };
+
+var username=document.getElementById('username').value;
+var password=document.getElementById('password').value;
+console.log('username');
+console.log('password');
+request.open('POST','http://swaaaaaathii.imad.hasura-app.io/login',true);
+request.setRequestHeader('Content-Type : application/json');
+request.send(JSON.stringify({username: username, password: password}));
+};
+
 /*var element=document.getElementById('main-text');
 element.innerHTML = 'new text';*/
 
