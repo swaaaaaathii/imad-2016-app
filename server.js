fabc7e052402d8e9e1fb81b5f8b921229ea899da
swaaaaaathii <swathi.n201097@gmail.com>
2016-11-22 21:03:00 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -109,7 +109,6 @@ app.post('/create-user',function(req,res){
     var username = req.body.username;
     var password = req.body.password;
     res.send(username);
-    res.send(password);
     var salt = crypto.randomBytes(128).toString('hex');
     var dbstring = hash(password, salt);
     pool.query('INSERT into "user" (username,password) values ($1,$2)',[username,dbstring],function(err,result){
