682024c16c2bb2e90a81301b13be343f0b1f4ddd
swaaaaaathii <swathi.n201097@gmail.com>
2016-11-24 18:33:41 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -106,6 +106,11 @@ app.get('/check-login',function(req,res){
     }
 });
 
+app.get('/logout',function(req,res){
+   delete req.session.auth;
+   res.send('You are logged out');
+});
+
 var pool = new Pool(config);
 
 function hash(input, salt){
