08152a7a1c3e875e29d7f6d623365ae34b5d34ca
swaaaaaathii <swathi.n201097@gmail.com>
2016-11-24 17:05:24 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -96,7 +96,7 @@ var pool = new Pool(config);
 
 function hash(input, salt){
     var hashed = crypto.pbkdf2Sync(input, salt, 10000, 512, 'sha512');
-    return [hashed.toString('hex')];
+    return ["pbkdf2", "100000", salt, hashed.toString('hex')].join('$');
 }
 
 app.get('/hash/:input',function(req,res){
