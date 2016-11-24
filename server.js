2f5c46abe066c32d545f11b52a264ba20f53256c
swaaaaaathii <swathi.n201097@gmail.com>
2016-11-24 16:11:20 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -96,7 +96,7 @@ app.get('/', function (req, res) {
 var pool = new Pool(config);
 
 function hash(input, salt){
-    var hashed = crypto.pbkdf2Sync(input, salt, 10000, 512, "sha512");
+    var hashed = crypto.pbkdf2Sync(input, salt, 10000, 512, 'sha512');
     return [hashed.toString('hex')];
 }
 
@@ -108,16 +108,17 @@ app.get('/hash/:input',function(req,res){
 app.post('/create-user',function(req,res){
     var username = req.body.username;
     var password = req.body.password;
-    password = password.toString('hex');
-    var salt = crypto.randomBytes(128).toString('hex');
-    var dbstring = hash(password, salt);
-    pool.query('INSERT into "user" (username,password) values ($1,$2)',[username,dbstring],function(err,result){
+    res.send(password);
+    //password = password.toString('hex');
+   // var salt = crypto.randomBytes(128).toString('hex');
+   // var dbstring = hash(password, salt);
+    /*pool.query('INSERT into "user" (username,password) values ($1,$2)',[username,dbstring],function(err,result){
         if(err){
             res.status(500).send(err.toString());
         }else{
             res.send('User successfully created : ' + username);
         }
-    });
+    });*/
 });
 
 app.post('/login',function(req,res){
