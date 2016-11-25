e3f1969d504c0285d89976b389696aa7991cb50f
swaaaaaathii <swathi.n201097@gmail.com>
2016-11-25 23:41:20 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -53,4 +53,13 @@ submit.onclick = function(){
             }
         }
     };
+var username=document.getElementById('username').value;
+var password=document.getElementById('password').value;
+var name=document.getElementById('name').value;
+var date=document.getElementById('date').value;
+var phno=document.getElementById('phno').value;
+var email=document.getElementById('email').value;
+request.open('POST','http://swaaaaaathii.imad.hasura-app.io/create-user',true);
+request.setRequestHeader('Content-Type', 'application/json');
+request.send(JSON.stringify({username: username, password: password, name: name, date: date, phno: phno, email: email}));
 };
