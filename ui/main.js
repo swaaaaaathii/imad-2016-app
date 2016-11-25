4cc201c79ecfb8257d2a9afbf9572f529eec4546
swaaaaaathii <swathi.n201097@gmail.com>
2016-11-25 23:53:04 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -37,31 +37,3 @@ function moveLeft()
 image.onclick = function () { 
     var interval = setInterval(moveLeft,5);
 };
-
-var submit=document.getElementById('submit_btn');
-
-submit.onclick = function(){
-    var request = new XMLHttpRequest();
-    request.onreadystatechange = function(){
-        if(request.readyState === XMLHttpRequest.DONE){
-            if(request.status===200){
-                alert('User created successfully');
-            }else if(request.status===403){
-                alert('User not created. Re-try');
-            }else if(request.status===500){
-                alert('Something wrong with the server');
-            }
-        }
-    };
-var username=document.getElementById('username').value;
-var password=document.getElementById('password').value;
-var name=document.getElementById('name').value;
-var date=document.getElementById('date').value;
-var phno=document.getElementById('phno').value;
-var email=document.getElementById('email').value;
-request.open('POST','http://swaaaaaathii.imad.hasura-app.io/create-user',true);
-request.setRequestHeader('Content-Type', 'application/json');
-console.log(username);
-console.log(password);
-request.send(JSON.stringify({username: username, password: password, name: name, date: date, phno: phno, email: email}));
-};
