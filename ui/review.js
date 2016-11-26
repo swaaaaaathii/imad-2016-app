bc9db286307125e59e073318b3310a5623ceeebd
swaaaaaathii <swathi.n201097@gmail.com>
2016-11-27 00:11:23 +0530

    [imad-console] Updates ui/review.js

--- ui/review.js
+++ ui/review.js
@@ -1,1 +1,23 @@
+var post=document.getElementById('post_btn');
+
+post.onclick = function(){
+    var request = new XMLHttpRequest();
+    request.onreadystatechange = function(){
+        if(request.readyState === XMLHttpRequest.DONE){
+            if(request.status===200){
+                alert('Review posted');
+            }else if(request.status===403){
+                alert('Unable to post. Re-try');
+            }else if(request.status===500){
+                alert('Something wrong with the server');
+            }
+        }
+    };
+var book_name=document.getElementById('book_name').value;
+var book_genre=document.getElementById('book_genre').value;
+var review=document.getElementById('review').value;
+request.open('POST','http://swaaaaaathii.imad.hasura-app.io/create-review',true);
+request.setRequestHeader('Content-Type', 'application/json');
+request.send(JSON.stringify({book_name: book_name, book_genre: book_genre, review: review}));
+};
 
