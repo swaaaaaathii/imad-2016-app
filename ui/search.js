a5c9bd339a5cec487843d5616304a985e70d25fc
swaaaaaathii <swathi.n201097@gmail.com>
2016-11-26 23:23:29 +0530

    [imad-console] Updates ui/search.js

--- ui/search.js
+++ ui/search.js
@@ -1,1 +1,21 @@
 var search=document.getElementById('search_btn');
+
+search.onclick = function(){
+    var request = new XMLHttpRequest();
+    request.onreadystatechange = function(){
+        if(request.readyState === XMLHttpRequest.DONE){
+            if(request.status===200){
+                alert('Search Successful');
+            }else if(request.status===403){
+                alert('Search Unsuccessful');
+            }else if(request.status===500){
+                alert('Something wrong with the server');
+            }
+        }
+    };
+var username=document.getElementById('book_name').value;
+request.open('POST','http://swaaaaaathii.imad.hasura-app.io/search-results',true);
+request.setRequestHeader('Content-Type', 'application/json');
+request.send(JSON.stringify({book_name: book_name}));
+location.href = '/';
+};
