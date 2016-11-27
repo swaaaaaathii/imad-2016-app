1892d9ce9daac52da810b9337353382bf4051478
swaaaaaathii <swathi.n201097@gmail.com>
2016-11-27 23:01:08 +0530

    [imad-console] Updates ui/viewresult.js

--- ui/viewresult.js
+++ ui/viewresult.js
@@ -1,6 +1,6 @@
 var currentrno = window.location.pathname.split('/')[3];
 var currentbname = window.location.pathname.split('/')[2];
-
-submit.onclick = function(){
-location.href = '/view-reviews/'+currentbname+'/'+currentrno;
+var next = document.getElementById('next');
+next.onclick = function(){
+location.href = '/view-reviews/'+currentbname+'/'+currentrno-1;
 };
