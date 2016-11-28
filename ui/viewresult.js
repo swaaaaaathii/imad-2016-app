a24bbb3c2e665e1149f0661d5a0c7994a29fcdd2
swaaaaaathii <swathi.n201097@gmail.com>
2016-11-28 16:43:51 +0530

    [imad-console] Updates ui/viewresult.js

--- ui/viewresult.js
+++ ui/viewresult.js
@@ -1,9 +1,10 @@
 var currentrno = window.location.pathname.split('/')[3];
 var currentbname = window.location.pathname.split('/')[2];
 var next = document.getElementById('next');
+var nextrno=currentrno-1;
 next.onclick = function(){
-    if(currentrno-1>=0){
-        location.href = '/view-reviews/'+currentbname+'/'+currentrno-1;
+    if(nextrno>=0){
+        location.href = '/view-reviews/'+currentbname+'/'+nextrno;
     }else{
         alert('No more reviews');
     }
