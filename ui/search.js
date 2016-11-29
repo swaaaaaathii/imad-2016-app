929d75bc5eb52cad33329a226cd181ca3b54e273
swaaaaaathii <swathi.n201097@gmail.com>
2016-11-29 12:56:46 +0530

    [imad-console] Updates ui/search.js

--- ui/search.js
+++ ui/search.js
@@ -2,6 +2,12 @@ var search=document.getElementById('search_btn');
 
 search.onclick = function(){
 var book_name=document.getElementById('bname').value;
+var i;
+for(i=0;book_name[i]!='/0';i++){
+    if(book_name[i]===' '){
+        book_name[i] = '-';
+    }
+}
 location.href='/view-reviews/'+book_name;
 };
 
