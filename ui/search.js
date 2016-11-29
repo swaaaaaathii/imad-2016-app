5b5f071b04ffade25176bed05fce591e3cf2beef
swaaaaaathii <swathi.n201097@gmail.com>
2016-11-29 13:29:17 +0530

    [imad-console] Updates ui/search.js

--- ui/search.js
+++ ui/search.js
@@ -1,6 +1,18 @@
 var search=document.getElementById('search_btn');
 
 search.onclick = function(){
+    var request = new XMLHttpRequest();
+    request.onreadystatechange = function(){
+        if(request.readyState === XMLHttpRequest.DONE){
+            if(request.status===200){
+                alert('Search Successful');
+            }else if(request.status===403){
+                alert('No review found');
+            }else if(request.status===500){
+                alert('Something wrong with the server');
+            }
+        }
+    };
 var book_name=document.getElementById('bname').value;
 book_name = book_name.split(" ").join("-").toLowerCase();
 location.href='/view-reviews/'+book_name;
