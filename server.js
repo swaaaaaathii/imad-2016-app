55f7e9a054bf28ee93a0507e5fca760cc4229ce9
swaaaaaathii <swathi.n201097@gmail.com>
2016-11-29 10:11:53 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -276,9 +276,7 @@ app.get('/view-reviews/:bookname/:rno',function(req,res){
 });
 
 app.get('/get-comments/:bookname', function (req, res) {
-   // make a select request
-   // return a response with the results
-   pool.query('SELECT comment.*, "user".username FROM review, comment, "user" WHERE review.book_name = $1 AND review.id = comment.review_id AND comment.user_id = "user".id ORDER BY comment.timestamp DESC', [req.params.articleName], function (err, result) {
+   pool.query('SELECT comment.*, "user".username FROM review, comment, "user" WHERE review.book_name = $1 AND review.id = comment.review_id AND comment.user_id = "user".id ORDER BY comment.timestamp DESC', [req.params.bookname], function (err, result) {
       if (err) {
           res.status(500).send(err.toString());
       } else {
